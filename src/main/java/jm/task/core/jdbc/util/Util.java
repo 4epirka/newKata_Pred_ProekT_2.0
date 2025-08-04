479cbb1a94c6492aedabfd7bacc68afa96f2bf80
4epirka <ivantar2000@mail.ru>
2025-08-04 17:49:56 +0300

    Initial commit

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,25 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private final static String URL = "jdbc:mysql://localhost:3306/sys";
+    private final static String USERNAME = "root";
+    private final static String PASSWORD = "VanekTar102030";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            System.out.println("Failed to connect to database...");
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
 }
